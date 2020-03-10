e39513a5281677708e7ae761763837b80758f5e0
Denis Karpov <DLKarpov@rolf.ru>
2020-03-10 18:36:53 +0300

    Pass 1.0.0.0. Change script/..  Changes to be committed: 	modified:   script/build.cmd 	modified:   script/cover.cmd

--- script/build.cmd
+++ script/build.cmd
@@ -1,12 +1,26 @@
-set GOROOT=c:\go
-set GOOS=windows
+:: Script for building an application for Windows
 
 cd ..
 
+:: Del old data
+IF EXIST bin RMDIR bin /Q /S
+IF EXIST pa.exe DEL pa.exe /Q /S
+IF EXIST debug.out DEL debug.out /Q /S
+IF EXIST test.out DEL test.out /Q /S
+
+:: Set env variable
+set GOROOT=c:\go
+set GOOS=windows
+
+:: Code format
+go fmt
+
+:: Code build x64
 set GOARCH=amd64
 go build -ldflags "-s -w" -i -v -o bin/release/64/pass.exe
 go build -race -i -v -o bin/debug/64/pass.exe
 
+:: Code build x86
 set GOARCH=386
 go build -ldflags "-s -w" -i -v -o bin/release/32/pass.exe
 go build -i -v -o bin/debug/32/pass.exe
