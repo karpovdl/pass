5212ec9e476780f0986c022987638e23622225bb
Denis Karpov <KarpovDL@hotmail.com>
2020-05-07 20:41:48 +0300

    Change script/cover

--- script/cover.cmd
+++ script/cover.cmd
@@ -7,9 +7,6 @@ IF EXIST cover.out DEL cover.out /Q /S
 IF EXIST cover.html DEL cover.html /Q /S
 
 :: Code cover
-cd ..
-
-:: Code cover
 go test -coverprofile cover.out
 go tool cover -html=cover.out -o cover.html
 
